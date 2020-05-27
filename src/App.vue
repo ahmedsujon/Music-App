c840e9a89dec2128db0090a9ef6cedc69d65f56f
ahmedsujon <ahmedsujon050@gmail.com>
2020-05-27 20:35:52 +0600

    Vue Js Music App

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,91 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      <h1>My Music</h1>
+    </header>
+    <main>
+      <section class="player">
+          <h2 class="song-title">{{ current.title }}- <span>{{ current.artist }}</span></h2>
+          <div class="control">
+            <button class="prev">Prev</button>
+            <button class="play"  v-if="!isplaying" @click="play">Play</button>
+            <button class="pause" v-else @click="pause">Pause</button>
+            <button class="next">Next</button>
+          </div>
+      </section>
+      <section class="playlist">
+       <h3>The PlayList</h3>
+       <button v-for="song in songs" :key="song.src" @click="play(song)" :class="song.src == current.src ? 'song playing' : 'song'">
+         {{ song.title }}-{{ song.artist }}
+       </button>
+      </section>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data(){
+    return{
+      current: {},
+      index: 0,
+      isplaying: false,
+      songs: [
+        {
+          title: 'Grateful',
+          artist: 'Neffex',
+          src: require('./assets/track_02.mp3')
+        },
+        {
+          title: 'Invencible',
+          artist: 'Def Kob',
+          src: require('./assets/track_03.mp3')
+        }
+      ],
+      player: new Audio()
+    }
+  },
+
+  methods: {
+    play (song) {
+      if(typeof song.src !="undefined"){
+        this.current.song;
+        this.player.src = this.current.src;
+      }
+      this.player.play();
+      this.ispalying = true; 
+  },
+
+    pause(){
+      this.player.pause();
+      this.ispalying = false;
+    }
+  },
+
+  created() {
+    this.current = this.songs[this.index];
+    this.player.src = this.current.src;
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body{
+  font-family: sans-serif;
+}
+header{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+  background-color: #212121;
+  color: #fff;
 }
 </style>
